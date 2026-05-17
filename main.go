24b13d59dde9ec1c40084366bb5eb187bf1bebe5
superharumaki <super.harumaki@gmail.com>
2026-05-17 13:52:31 +0900

    Refactor post creation and deletion logic

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
-resp, err := client.CreatePost(authCtx, &application_apiv1.CreatePostRequest{
-    Post: &application_apiv1.Post{
-        Text: "削除テスト",
-    },
-})
-if err != nil {
-    log.Fatal("create error:", err)
+package main
+
+import (
+    "context"
+    "log"
+    "fmt"
+)
+
+func main() {
+    authCtx := context.Background()
+
+    resp, err := client.CreatePost(authCtx, &application_apiv1.CreatePostRequest{
+        Post: &application_apiv1.Post{
+            Text: "削除テスト",
+        },
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("投稿成功")
+
+    _, err = client.DeletePost(authCtx, &application_apiv1.DeletePostRequest{
+        PostId: resp.Post.PostId,
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("削除成功")
 }
-
-if resp == nil || resp.Post == nil {
-    log.Fatal("create response is nil")
-}
-
-postID := resp.Post.PostId
-fmt.Println("投稿成功 PostID:", postID)
-
-_, err = client.DeletePost(authCtx, &application_apiv1.DeletePostRequest{
-    PostId: postID,
-})
-if err != nil {
-    log.Fatal("delete error:", err)
-}
-
-fmt.Println("削除成功")
