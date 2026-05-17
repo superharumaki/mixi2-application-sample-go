e033f3d2ccb23e861695ae8081d1c7fc3f73ec92
superharumaki <super.harumaki@gmail.com>
2026-05-17 13:54:08 +0900

    Add gRPC client connection to ApplicationService

--- main.go
+++ main.go
@@ -4,9 +4,24 @@ import (
     "context"
     "log"
     "fmt"
+
+    application_apiv1 "github.com/mixigroup/mixi2-application-sample-go/application/apiv1"
+
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
+    conn, err := grpc.Dial(
+        "application.mixi.social:443",
+        grpc.WithTransportCredentials(insecure.NewCredentials()),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    client := application_apiv1.NewApplicationServiceClient(conn)
     authCtx := context.Background()
 
     resp, err := client.CreatePost(authCtx, &application_apiv1.CreatePostRequest{
