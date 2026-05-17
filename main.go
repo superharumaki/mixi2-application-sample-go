35689a994d0e4fd5d6bd1bfff2ab7fe15ad4177d
superharumaki <super.harumaki@gmail.com>
2026-05-17 13:50:10 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,94 +1,19 @@
-package main
-
-import (
-	"context"
-	"crypto/tls"
-	"log"
-	"math/rand"
-	"time"
-
-	"github.com/mixigroup/mixi2-application-sample-go/config"
-	"github.com/mixigroup/mixi2-application-sdk-go/auth"
-	application_apiv1 "github.com/mixigroup/mixi2-application-sdk-go/gen/go/social/mixi/application/service/application_api/v1"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-)
-
-type Song struct {
-	Title string
-	URL   string
+resp, err := client.CreatePost(authCtx, &application_apiv1.CreatePostRequest{
+    Post: &application_apiv1.Post{
+        Text: "削除テスト",
+    },
+})
+if err != nil {
+    log.Fatal(err)
 }
 
-func main() {
+fmt.Println("投稿成功")
 
-	cfg := config.GetConfig()
-
-	authenticator, err := auth.NewAuthenticator(
-		cfg.ClientID,
-		cfg.ClientSecret,
-		cfg.TokenURL,
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	apiConn, err := grpc.NewClient(
-		cfg.APIAddress,
-		grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})),
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer apiConn.Close()
-
-	client := application_apiv1.NewApplicationServiceClient(apiConn)
-
-	authCtx, err := authenticator.AuthorizedContext(context.Background())
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	songs := []Song{
-		{
-			Title: "シクラメンのかほり",
-			URL:   "https://www.youtube.com/results?search_query=布施明+シクラメンのかほり",
-		},
-		{
-			Title: "君は薔薇より美しい",
-			URL:   "https://www.youtube.com/results?search_query=布施明+君は薔薇より美しい",
-		},
-		{
-			Title: "積木の部屋",
-			URL:   "https://www.youtube.com/results?search_query=布施明+積木の部屋",
-		},
-		{
-			Title: "MY WAY",
-			URL:   "https://www.youtube.com/results?search_query=布施明+MY+WAY",
-		},
-	}
-
-	rand.Seed(time.Now().UnixNano())
-
-	song := songs[rand.Intn(len(songs))]
-
-	text := "今日の布施明🎤\n\n" +
-		song.Title +
-		"\n\n▶ " +
-		song.URL +
-		"\n\n#布施明"
-
-	_, err = client.CreatePost(authCtx, &application_apiv1.CreatePostRequest{
-		Text: text,
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("投稿成功:", song.Title)
+_, err = client.DeletePost(authCtx, &application_apiv1.DeletePostRequest{
+    PostId: resp.Post.PostId,
+})
+if err != nil {
+    log.Fatal(err)
 }
+
+fmt.Println("削除成功")
